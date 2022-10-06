ca4d72d70383b4ce3883aa570ee3fd18ad5d7924
Hai Long <hailongkc1203@gmail.com>
2022-10-06 21:58:38 +0700

    To_Do_List_HaiLong

---
+++ HaiLong/MERN/NodeJs/src/controllers/boardController.js
@@ -0,0 +1,17 @@
+import { postNewBoardService } from "../services/boardService";
+
+const getNewBoard = (req, res) => {
+    return res.send("test api from controller");
+}
+const postNewBoard = async (req, res) => {
+    console.log("test body", req.body);
+    let result = await postNewBoardService(req.body);
+    return res.status(200).json({
+        msg: "ok",
+        data: result
+    })
+}
+
+module.exports = {
+    postNewBoard, getNewBoard
+}
