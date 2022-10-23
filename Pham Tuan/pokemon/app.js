af2451a7b342f29cc280cca551f976f4c7a2eec7
phamtuan101001 <yukikami101001@gmail.com>
2022-10-23 16:55:39 +0700

    Search Pokemon

---
+++ Pham Tuan/pokemon/app.js
@@ -0,0 +1,25 @@
+const namePokemon = document.querySelector('input');
+const searchPokemon = document.querySelector('button');
+const display = document.querySelector('.display');
+searchPokemon.addEventListener("click", () => {
+    let pokemon = namePokemon.value;
+    let URL = 'https://pokeapi.co/api/v2/pokemon/';
+    let API = URL + pokemon;
+    console.log(API);
+async function getPokemon(){
+   const response = await fetch(API);
+   const data = await response.json();
+    return data;
+}
+    getPokemon().then(data =>{
+        display.innerHTML = `
+        <li> name: ${data.name}</li>
+        <li> heigh: ${data.height} dm</li>
+        <li> weight: ${data.weight} hg</li>
+        <li> species: ${data.types[0].type.name}</li>
+        <img src="${data.sprites.front_default}" alt="anhPokemon">
+        <img src="${data.sprites.back_default}" alt="anhPokemon">
+        `
+    })
+    
+})
